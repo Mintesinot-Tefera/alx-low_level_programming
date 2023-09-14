09f5a1924fcad589972962736623f15b876a38de
Mintesinot-Tefera <mintesinotteferamt@gmail.com>
2023-09-14 12:41:12 -0700

    taski0_updated8

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,4 @@
- /*
- * File: 0-putchar.c 
- *
- */
-
 #include "main.h" 
-
- /** 
- * main-print putchar followed by a new line
- * 
- *Return :always 0
- */
 int main(void) 
 {
 
@@ -24,4 +13,5 @@ _putchar('r');
 _putchar('\n');
 
 return (0);
-} 
+}
+
